e530514cf000106badc007c0a70c6d1e79e65436
Marco Martinez <marco.antoni.martinez@gmail.com>
2025-08-15 22:30:06 +0000

    Added list of currencies

--- app.rb
+++ app.rb
@@ -1,9 +1,30 @@
 require "sinatra"
 require "sinatra/reloader"
+require "json"
+require "dotenv/load"
+require "http"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
-end
+  
+  list_url = "https://api.exchangerate.host/list?access_key=" + ENV.fetch("EXCHANGE_RATE_KEY")
+
+  #get response for location
+  resp = HTTP.get(list_url)
+
+  raw_response = resp.to_s
+
+  parsed_response = JSON.parse(raw_response)
+
+  @results = parsed_response.fetch("currencies").keys
+
+  
+  
+  erb(:home)
+  
+ end
+
+
+
+
+
+#exc_url = "https://https://api.exchangerate.host/convert?from=" + curr_from + "&to="  + curr_to  + "&amount=1&access_key=" + ENV.fetch("EXCHANGE_RATE_KEY") 
