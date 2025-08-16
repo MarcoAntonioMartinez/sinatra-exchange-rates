e3ca5810a2c75fc6cbbe8e53df6942e3aab4721c
Marco Martinez <marco.antoni.martinez@gmail.com>
2025-08-16 01:49:24 +0000

    Added to and exchange views

--- app.rb
+++ app.rb
@@ -5,26 +5,52 @@ require "dotenv/load"
 require "http"
 
 get("/") do
-  
-  list_url = "https://api.exchangerate.host/list?access_key=" + ENV.fetch("EXCHANGE_RATE_KEY")
 
-  #get response for location
+@results = api_list
+  erb(:home)
+  
+end
+
+get("/:currency") do
+
+  @currency = params.fetch("currency")
+
+  @results = api_list
+
+  erb(:to)
+
+end
+
+get("/:curr_from/:curr_to") do
+
+  @curr_from = params.fetch("curr_from")
+  @curr_to = params.fetch("curr_to")
+
+  exc_url = "https://api.exchangerate.host/convert?from=" + @curr_from + "&to="  + @curr_to  + "&amount=1&access_key=" + ENV.fetch("EXCHANGE_RATE_KEY") 
+
+#get response for exchange of currency
+  resp = HTTP.get(exc_url)
+
+  raw_response = resp.to_s
+
+  parsed_response = JSON.parse(raw_response)
+
+  @result = parsed_response.fetch("result")
+
+  erb(:exchange)
+end
+
+
+def api_list 
+
+list_url = "https://api.exchangerate.host/list?access_key=" + ENV.fetch("EXCHANGE_RATE_KEY")
+
+  #get response for list of currency
   resp = HTTP.get(list_url)
 
   raw_response = resp.to_s
 
   parsed_response = JSON.parse(raw_response)
 
-  @results = parsed_response.fetch("currencies").keys
-
-  
-  
-  erb(:home)
-  
- end
-
-
-
-
-
-#exc_url = "https://https://api.exchangerate.host/convert?from=" + curr_from + "&to="  + curr_to  + "&amount=1&access_key=" + ENV.fetch("EXCHANGE_RATE_KEY") 
+  results = parsed_response.fetch("currencies").keys
+end
